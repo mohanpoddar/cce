aa94367a4a2c5a043f5e7a88ac2f6446946afb5c
Mohan Poddar <contactme.mohan@gmail.com>
2025-03-08 14:32:19 +0530

    Add host sepecific input files here

--- ubuntu-local-setup/roles/home_ubuntu_setup/files/rsync_daily.sh
+++ ubuntu-local-setup/roles/home_ubuntu_setup/files/rsync_daily.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
-#PS1=`ps -ef | grep rsync | grep -v grep | wc -l`
-PS1=`ps -ef | egrep "rsync -avP /opt/ccpldata/ccplnewdata" | grep -v grep | wc -l`
-BKP_LOC_SRC='/opt/ccpldata/ccplnewdata'
-#BKP_LOC_DST='/opt/backup/backup_of_opt_ccpldata_ccplnewdata_20-12-2024/'
-BKP_LOC_DST='/opt/backup/backup_of_opt_ccpldata_ccplnewdata_latest/'
-RSYNC_EMAIL_FILE='/home/cce/rayo/scripts/github/cce/ubuntu-local-setup/roles/home_ubuntu_setup/files/cceplrsyncmail.py'
 
+# Set HOST variable based on hostname
+if [ "$(hostname)" = "av-pc" ]; then
+    HOST="GREATERNOIDA"
+    PS1=`ps -ef | egrep "rsync -avP /opt/ccpldata/ccplnewdata" | grep -v grep | wc -l`
+    BKP_LOC_SRC='/opt/ccpldata/ccplnewdata'
+    BKP_LOC_DST='/opt/backup/backup_of_opt_ccpldata_ccplnewdata_latest/'
+    RSYNC_EMAIL_FILE='/home/cce/rayo/scripts/github/cce/ubuntu-local-setup/roles/home_ubuntu_setup/files/cceplrsyncmail.py'
+
+elif [ "$(hostname)" = "503S" ]; then
+    HOST="VASHUNDHARA"
+    PS1=`ps -ef | egrep "rsync -avP /opt/ccepldata/cceplserver" | grep -v grep | wc -l`
+    BKP_LOC_SRC='/opt/ccepldata/cceplserver'
+    BKP_LOC_DST='/opt/backup/backup_of_opt_ccpldata_cceplserver_latest/'
+    RSYNC_EMAIL_FILE='/home/cce/rayo/scripts/github/cce/ubuntu-local-setup/roles/home_ubuntu_setup/files/cceplrsyncmail.py'
+
+elif [ "$(hostname)" = "mylearnersepoint" ]; then
+    HOST="MYLEARNERSEPOINT"
+    PS1=`ps -ef | egrep "rsync -avP /opt/ccpldata/mylearnersepoint" | grep -v grep | wc -l`
+    BKP_LOC_SRC='/opt/ccpldata/mylearnersepoint'
+    BKP_LOC_DST='/opt/backup/mylearnersepoint/'
+    RSYNC_EMAIL_FILE='/home/cce/rayo/scripts/github/cce/ubuntu-local-setup/roles/home_ubuntu_setup/files/cceplrsyncmail.py'
+
+else
+    HOST="UNKNOWN"
+fi
+
+echo "Hostname: $(hostname)"
+echo "Number of rsync process: $PS1"
+echo "Host Location: $HOST"
 echo "Backup Source Location: $BKP_LOC_SRC"
 echo "Backup Destination Location: $BKP_LOC_DST"
+echo "Rsync Email File: $RSYNC_EMAIL_FILE"
 
-echo "Number of rsync process: $PS1"
+# #PS1=`ps -ef | grep rsync | grep -v grep | wc -l`
+# PS1=`ps -ef | egrep "rsync -avP /opt/ccpldata/ccplnewdata" | grep -v grep | wc -l`
+# BKP_LOC_SRC='/opt/ccpldata/ccplnewdata'
+# #BKP_LOC_DST='/opt/backup/backup_of_opt_ccpldata_ccplnewdata_20-12-2024/'
+# BKP_LOC_DST='/opt/backup/backup_of_opt_ccpldata_ccplnewdata_latest/'
+# RSYNC_EMAIL_FILE='/home/cce/rayo/scripts/github/cce/ubuntu-local-setup/roles/home_ubuntu_setup/files/cceplrsyncmail.py'
 
+# echo "Backup Source Location: $BKP_LOC_SRC"
+# echo "Backup Destination Location: $BKP_LOC_DST"
+
+# echo "Number of rsync process: $PS1"
 
 ccersync () {
 
